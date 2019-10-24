cebbf40efb0c9335fd4e9635acef03f9016165e5
datacruiser <phy.zju@gmail.com>
2019-10-24 19:36:53 +0800

    markdown

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -7,22 +7,22 @@
     (save-excursion
       (imenu--generic-function patterns))))
 
-;;(defun markdown-mode-hook-setup ()
+;(defun markdown-mode-hook-setup ()
   ;; Stolen from http://stackoverflow.com/a/26297700
   ;; makes markdown tables saner via orgtbl-mode
   ;; Insert org table and it will be automatically converted
   ;; to markdown table
-  ;(unless (featurep 'org-table) (require 'org-table))
+ ; (unless (featurep 'org-table) (require 'org-table))
   ;(defun cleanup-org-tables ()
    ; (save-excursion
     ;  (goto-char (point-min))
      ; (while (search-forward "-+-" nil t) (replace-match "-|-"))))
-  ;(add-hook 'after-save-hook 'cleanup-org-tables nil 'make-it-local)
+ ; (add-hook 'after-save-hook 'cleanup-org-tables nil 'make-it-local)
   ;(orgtbl-mode 1) ; enable key bindings
   ;; don't wrap lines because there is table in `markdown-mode'
- ; (setq truncate-lines t)
+  ;(setq truncate-lines t)
  ; (setq imenu-create-index-function 'markdown-imenu-index))
-(add-hook 'markdown-mode-hook 'markdown-mode-hook-setup)
+;(add-hook 'markdown-mode-hook 'markdown-mode-hook-setup)
 
 (eval-after-load 'markdown-mode
   '(progn
