67c7d20f047d9c40612ae0e7c04bbdc362222815
datacruiser <phy.zju@gmail.com>
2019-12-01 20:20:10 +0800

    使用emacs-mac 并调整meta键以及mac本身的快捷键

--- init.el
+++ init.el
@@ -189,6 +189,23 @@
 (put 'erase-buffer 'disabled nil)
 
 
+;;using the option as meta 
+;; Keybonds
+(global-set-key [(hyper a)] 'mark-whole-buffer)
+(global-set-key [(hyper v)] 'yank)
+(global-set-key [(hyper c)] 'kill-ring-save)
+(global-set-key [(hyper s)] 'save-buffer)
+(global-set-key [(hyper l)] 'goto-line)
+(global-set-key [(hyper w)]
+                (lambda () (interactive) (delete-window)))
+(global-set-key [(hyper z)] 'undo)
+(global-set-key [(hyper q)]'save-buffers-kill-emacs)
+
+;; mac switch meta key
+(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'hyper)
+
+;;configuration for the liberime input 
 (setq load-path (cons (file-truename "~/.emacs.d/") load-path))
 
 (require 'pyim)
