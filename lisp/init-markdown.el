4f2e43f06b224132dcc95863c9172a431357a38d
datacruiser <phy.zju@gmail.com>
2020-08-31 23:04:31 +0800

    fix markdown-mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -17,26 +17,21 @@
      ; (while (search-forward "-+-" nil t) (replace-match "-|-"))))
  ; (add-hook 'after-save-hook 'cleanup-org-tables nil 'make-it-local)
   ;(orgtbl-mode 1) ; enable key bindings
-  (my-ensure 'org-table)
-  (defun cleanup-org-tables ()
-    (save-excursion
-      (goto-char (point-min))
-      (while (search-forward "-+-" nil t) (replace-match "-|-"))))
-  (add-hook 'after-save-hook 'cleanup-org-tables nil 'make-it-local)
-  (orgtbl-mode 1) ; enable key bindings
+;  (my-ensure 'org-table)
+ ; (defun cleanup-org-tables ()
+  ;  (save-excursion
+   ;   (goto-char (point-min))
+    ;  (while (search-forward "-+-" nil t) (replace-match "-|-"))))
+ ; (add-hook 'after-save-hook 'cleanup-org-tables nil 'make-it-local)
+ ; (orgtbl-mode 1) ; enable key bindings
   ;; don't wrap lines because there is table in `markdown-mode'
   ;(setq truncate-lines t)
  ; (setq imenu-create-index-function 'markdown-imenu-index))
 ;(add-hook 'markdown-mode-hook 'markdown-mode-hook-setup)
 
-(eval-after-load 'markdown-mode
-  '(progn
-     ;; `pandoc' is better than obsolete `markdown'
-     (when (executable-find "pandoc")
-       (setq markdown-command "pandoc -f markdown -t html -s --mathjax")))) ;; add mathjax support
 (with-eval-after-load 'markdown-mode
-  ;; `pandoc' is better than obsolete `markdown'
+     ;; `pandoc' is better than obsolete `markdown'
   (when (executable-find "pandoc")
-    (setq markdown-command "pandoc -f markdown")))
+    (setq markdown-command "pandoc -f markdown -t html -s --mathjax")));; add mathjax support
 
 (provide 'init-markdown)
